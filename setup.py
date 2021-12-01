319139f05b8bd460f72817c0a5417962e7f96b1d
Boris Timofeenko <timbori@mail.ru>
2021-12-01 22:57:20 +0300

    Fix for Python3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from __future__ import print_function
 
 from setuptools import setup, Extension
 
-USE_CYTHON = False
+USE_CYTHON = True
 
 DISTNAME = 'lsh'
 DESCRIPTION = 'A library for performing shingling and LSH for python.'
@@ -28,7 +28,7 @@ extensions = [Extension("lsh.cMinhash",
 if USE_CYTHON:
     from Cython.Build import cythonize
 
-    extensions = cythonize(extensions)
+    extensions = cythonize(extensions, force=True)
 
 install_deps = ['numpy', 'cython>=0.24.1']
 test_deps = ['coverage>=4.0.3', 'pytest>=3.0', ]
